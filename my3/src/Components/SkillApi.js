1ae203d8a22b9204eb5b3d423c750d85d305c2fd
AnkushKirar <ankushkirar777@gmail.com>
2023-12-17 18:40:26 +0530

    resolve issue

--- my3/src/Components/SkillApi.js
+++ my3/src/Components/SkillApi.js
@@ -3,57 +3,56 @@
 const Skill_data = [
     {id:1,
     Tittle: "React",
-    Description : 
-    "Mastered React with Meta—because who needs a superhero when you have the power of virtual DOM! Created more projects than I can count (check 'em out in the project section), making React my Iron Man suit in the coding universe. 💻⚛️ #ReactAvenger",
+    Description : "React Avenger, coding universe conqueror",
     Images: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/640px-React-icon.svg.png"
 
 
     },
     {id:2,
         Tittle: "JavaScript",
-        Description : "JavaScript, my first love, swept me off my feet during those college coding rendezvous. From the first 'Hello World,' it was clear - my heart belonged to the language of romance and code. 💖🌐 #JavaScriptRomance",
+        Description : "JavaScript swept my college coding. 💖🌐 #JavaScriptRomance",
        Images: "https://seeklogo.com/images/J/javascript-js-logo-2949701702-seeklogo.com.png"
     
     
     },
     {id:3,
         Tittle: "Java",
-        Description : "Java, my new coding crush, just walked into my programming life. Still figuring out our rhythm, but it's a beginner's dance full of excitement. Here's to the fresh journey of learning Java! 💻🕺 #JavaJourney",
+        Description : "Java, my coding crush. 💻🕺 #JavaJourney",
        Images: "https://www.svgrepo.com/show/349418/java.svg"
     
     
     },
     {id:4,
         Tittle: "CSS/CSS3",
-        Description : "CSS is my kind of makeup for the web—I may be low in real makeup skills, but when it comes to styling websites, I'm the Picasso of pixels. Who needs foundation when you've got fancy divs and stylish selectors? 💄💻😄 #CSSMakeupMaestro",
+        Description : "CSS, web makeup Picasso. 💄💻😄 #CSSMakeupMaestro",
        Images: "https://www.svgrepo.com/show/303263/css3-logo.svg"
     
     
     },
     {id:5,
         Tittle: "tailwind",
-        Description : "Started learning Tailwind to level up my CSS game, and now I'm head over heels in love with it. It's like the perfect fashion makeover for my web styling adventures! 💅💻❤️ #TailwindRomance",
+        Description : "Tailwind, CSS game changer. 💅💻❤️ #TailwindRomance",
        Images: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/1024px-Tailwind_CSS_Logo.svg.png"
     
     
     },
     {id:6,
         Tittle: "HTML",
-        Description : "Back in school, I first met HTML like an innocent child discovering a treasure chest of code. Started with the basics, then dabbled in some 'hacking' adventures (well, sort of!). Now, call me an HTML expert—I've come a long way from those playful school days! 👶💻🚀 #HTMLExplorer",
+        Description : "HTML, coding treasure discovery. 👶💻🚀 #HTMLExplorer",
        Images: "https://www.svgrepo.com/show/373669/html.svg"
     
     
     },
     {id:7,
         Tittle: "UX&UI",
-        Description : "Used to think UX and UI were like two peas in a pod—indistinguishable. But oh, the enlightenment! Now I see the nuanced dance between user experience and user interface. It's like discovering chocolate and realizing there's dark, milk, and white. 🍫🎨 #UXUIRevelation",
+        Description : "Discovering UX and UI nuances, like exploring dark, milk, and white chocolate.",
        Images: "https://www.sstechsystem.com/wp-content/uploads/2018/11/ui-ux-design.png"
     
     
     },
     {id:8,
         Tittle: "gitHub",
-        Description : "Meet my personal manager, my Jarvis—saving my days and channeling my inner Tony Stark vibes. Because every coding superhero deserves an AI sidekick! 💻⚡",
+        Description : "Meet my coding manager, Jarvis—channeling inner Tony Stark vibes. Every superhero needs an AI sidekick! 💻⚡",
        Images: "https://logos-world.net/wp-content/uploads/2020/11/GitHub-Symbol.png"
     
     
